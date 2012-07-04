1991c344b4913d5d3d59b80538bd7d6b51451a4e
dongfangz050 <dongfangz050@gmail.com>
2012-07-04 12:47:02 -0400

    Changed From Factory Method to Abstract Factory design pattern for DAO

--- MarketPlace/src/dao/AbstractDAOFactory.java
+++ MarketPlace/src/dao/AbstractDAOFactory.java
@@ -1,6 +1,9 @@
 package dao;
 
 public interface AbstractDAOFactory {
-	abstract AbstractDAO getDAO(String daoType);
+	//abstract AbstractDAO getDAO(String daoType);
 	
+	abstract FileDAO getFileDAO();
+	abstract ItemDAO getItemDAO();
+	abstract UserDAO getUserDAO();
 }
