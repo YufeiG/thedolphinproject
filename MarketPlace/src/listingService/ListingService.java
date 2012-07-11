25af246f33e832e3ab81df500b595d90177eacad
tinaliang22 <tinaliang22@hotmail.com>
2012-07-11 16:59:37 -0400

    add recommendation service and listing service

--- MarketPlace/src/listingService/ListingService.java
+++ MarketPlace/src/listingService/ListingService.java
@@ -1,5 +1,25 @@
 package listingService;
 
-public class ListingService {
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Item;
+import model.Tag;
+
+public class ListingService {
+	public boolean createItem(Item i){
+		return false;
+	}
+	
+	public List<Item> findItems(List<Tag> list, String category){
+		return new ArrayList<Item>();
+	}
+	
+	public boolean editItem(Item i){
+		return false;
+	}
+	
+	public boolean deleteItem(Item i){
+		return false;
+	}
 }
