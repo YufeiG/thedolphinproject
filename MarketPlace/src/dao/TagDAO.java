6bf57edc6ed6d35e33e58aa771e8edbc719a331d
Kathy Ma <kwlma@kathy-eeepc.(none)>
2012-07-11 16:51:11 -0400

    added dao interfaces

--- MarketPlace/src/dao/TagDAO.java
+++ MarketPlace/src/dao/TagDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
-public interface TagDAO {
+import java.util.List;
+import model.Tag;
 
+public interface TagDAO {
+	public String getTag(String tagName);
+	public boolean deleteTag(String tagName);
+	public boolean createTag(String tagName);
+	public List<Tag> getTags();
 }
