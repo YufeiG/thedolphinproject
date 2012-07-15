d6833d07249dab12e55a7f422ce7bf583ee8c5b9
tinaliang22 <tinaliang22@hotmail.com>
2012-07-15 15:54:43 -0400

    implement recency search processing

--- MarketPlace/src/listingService/RecencySearchProcessing.java
+++ MarketPlace/src/listingService/RecencySearchProcessing.java
@@ -1,5 +1,7 @@
 package listingService;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import model.Item;
@@ -8,6 +10,29 @@ public class RecencySearchProcessing implements SearchProcessing  {
 
 	@Override
 	public List<Item> process(List<Item> list) {
+		
+		if(list == null || list.size() <= 1){
+			return list;
+		}
+		
+		Collections.sort(list, new Comparator<Item>() {
+
+			@Override
+			public int compare(Item o1, Item o2) {
+				if(o1.getTimePosted().before(o2.getTimePosted())){
+					return -1;
+				}
+				else if(o1.getTimePosted().equals(o2.getTimePosted())){
+					return 0;
+				}
+				else{
+					return 1;
+				}
+			}
+				
+			});
+		
+		
 		return list;
 	}
 
