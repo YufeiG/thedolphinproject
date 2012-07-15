7c05e973752bb848bf2bd95e41d64d66012541c2
tinaliang22 <tinaliang22@hotmail.com>
2012-07-15 15:19:35 -0400

    create main scheduler and wish list match thread when server starts

--- MarketPlace/src/controller/SchedulerInitializationAction.java
+++ MarketPlace/src/controller/SchedulerInitializationAction.java
@@ -2,10 +2,13 @@ package controller;
 
 import javax.servlet.http.HttpServlet;
 
+import scheduler.MainScheduler;
+
 public class SchedulerInitializationAction extends HttpServlet {
 	
 	@Override
 	public void init(){
-		
+		MainScheduler scheduler = MainScheduler.getInstance();
+		scheduler.createSchedulerThread();
 	}
 }
