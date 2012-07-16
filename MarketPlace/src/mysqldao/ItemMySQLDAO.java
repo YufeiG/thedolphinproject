63b28d169dca5b395505249859f48604f9a5a0d5
Kathy Ma <kwlma@kathy-eeepc.(none)>
2012-07-16 15:46:10 -0400

    tag dao done

--- MarketPlace/src/mysqldao/ItemMySQLDAO.java
+++ MarketPlace/src/mysqldao/ItemMySQLDAO.java
@@ -11,117 +11,105 @@ import model.Tag;
 import dao.AbstractDAO;
 import dao.ItemDAO;
 
-public class ItemMySQLDAO extends AbstractDAO implements ItemDAO{
+public class ItemMySQLDAO extends AbstractDAO implements ItemDAO {
+
+	public ItemMySQLDAO() {
+		mConnection = createConnection();
+	}
+
+	private Item getItemObj(ResultSet rs) throws SQLException {
+		Item item = new Item(rs.getLong("itemid"), rs.getString("title"),
+				rs.getInt("category"), rs.getLong("userid"),
+				rs.getString("description"), rs.getInt("sold"),
+				rs.getDate("avail_start"), rs.getDate("avail_end"),
+				rs.getFloat("price_low"), rs.getFloat("price_high"),
+				rs.getInt("popularity"), rs.getDate("time_added"),
+				rs.getDate("time_mod"));
+		return item;
+	}
+
+	public Item getItem(long itemid) throws SQLException {
+		String query = "SELECT * FROM items WHERE itemid=" + itemid;
+		ResultSet rs = execSql(query);
+
+		return getItemObj(rs);
+	}
+
+	public boolean deleteItem(Item item) throws SQLException {
+		String query = "DELETE FROM items WHERE itemid = " + item.getItemid();
+		execSql(query);
+
+		return true;
+	}
+
+	public boolean editItem(Item item) throws SQLException {
+		String query = "UPDATE users"
+				+ String.format("SET title='%s', " + "category=%d, "
+						+ "description=%s, " + "sold=%d, " + "avail_start=%s, "
+						+ "avail_end=%s, " + "price_low=%f, "
+						+ "price_high=%f, " + "popularity=%d, "
+						+ "time_mod='%s'", item.getTitle(), item.getCategory(),
+						strIsNull(item.getDescription()), item.getSold(),
+						strIsNull(item.getAvailStart().toString()),
+						strIsNull(item.getAvailEnd().toString()),
+						item.getPriceLow(), item.getPriceHigh(),
+						item.getPopularity(), item.getTimeModified().toString());
+		execSql(query);
+		return true;
+	}
+
+	public boolean createItem(Item item, List<Tag> tags) throws SQLException {
 		
-		public ItemMySQLDAO(){
-			mConnection = createConnection();
-		}
-		
-		private Item getItemObj(ResultSet rs) throws SQLException {
-			Item item = null;
-			if (rs.next()) {
-				item = new Item(
-						rs.getLong("itemid"),
-						rs.getString("title"),
-						rs.getInt("category"),
-						rs.getLong("userid"),
-						rs.getString("description"),
-						rs.getInt("sold"),
-						rs.getDate("avail_start"),
-						rs.getDate("avail_end"),
-						rs.getFloat("price_low"),
-						rs.getFloat("price_high"),
-						rs.getInt("popularity"),
-						rs.getDate("time_added"),
-						rs.getDate("time_mod"));
-			}
-			return item;
-		}
+		String query = "INSERT INTO items (title, category, userid,, description, "
+				+ "sold, avail_start, avail_end, price_low, price_high, popularity, "
+				+ "time_added, time_mod) "
+				+ String.format(
+						"VALUES ('%s',%d, %d, %s, '%d', %s, %s, %f, %f, %d, '%s', '%s')",
+						item.getTitle(), 
+						item.getCategory(), 
+						item.getUserid(),
+						strIsNull(item.getDescription()), 
+						item.getSold(),
+						strIsNull(item.getAvailStart().toString()),
+						strIsNull(item.getAvailEnd().toString()),
+						item.getPriceLow(), 
+						item.getPriceHigh(),
+						item.getPopularity(), 
+						item.getTimeAdded().toString(),
+						item.getTimeModified().toString());
 
-		public Item getItem(long itemid) throws SQLException {
-			String query = "SELECT * FROM items WHERE itemid=" + itemid;
-			ResultSet rs = execSql(query);
-			
-			return getItemObj(rs);
-		}
+		execSql(query);
 
-		public boolean deleteItem(Item item) throws SQLException {
-			String query = "DELETE FROM items WHERE itemid = " + item.getItemid();
-			execSql(query);
-			
-			return true;
-		}
+		return true;
+	}
 
-		public boolean editItem(Item item) throws SQLException {
-			String query =
-					"UPDATE users" +
-					String.format("SET title='%s', " +
-							"category=%d, " +
-							"description=%s, " +
-							"sold=%d, " +
-							"avail_start=%s, " +
-							"avail_end=%s, " +
-							"price_low=%f, " +
-							"price_high=%f, " +
-							"popularity=%d, " +
-							"time_mod='%s'",
-							item.getTitle(), item.getCategory(),
-							strIsNull(item.getDescription()), item.getSold(),
-							strIsNull(item.getAvailStart().toString()), 
-							strIsNull(item.getAvailEnd().toString()),
-							item.getPriceLow(), item.getPriceHigh(), item.getPopularity(),
-							item.getTimeModified().toString());
-			execSql(query);
-			return true;
+	public List<Item> getItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Item> getItems(List<Tag> tags) throws SQLException {
+		String strTags = "";
+		int listSize = tags.size();
+
+		if (listSize > 0)
+			strTags = "'" + tags.get(0).toString() + "'";
+
+		for (int i = 1; i < listSize; i++) {
+			strTags += ", '" + tags.get(0).toString() + "'";
 		}
 
-		public boolean createItem(Item item, List<Tag> tags) throws SQLException {
-			
-			String query = 
-					"INSERT INTO items (title, category, userid,, description, " +
-					"sold, avail_start, avail_end, price_low, price_high, popularity, " +
-					"time_added, time_mod) " +
-					String.format("VALUES ('%s',%d, %d, %s, '%d', %s, %s, %f, %f, %d, '%s', '%s')",
-							item.getTitle(), item.getCategory(), item.getUserid(),
-							strIsNull(item.getDescription()), item.getSold(),
-							strIsNull(item.getAvailStart().toString()), 
-							strIsNull(item.getAvailEnd().toString()),
-							item.getPriceLow(), item.getPriceHigh(), item.getPopularity(),
-							item.getTimeAdded().toString(),
-							item.getTimeModified().toString());
-			
-			execSql(query);
+		String query = "SELECT * FROM items i, tags t, item_tags r "
+				+ "WHERE i.itemid = r.itemid AND t.tagid = item_tags.tagid "
+				+ "AND t.tag_name IN (" + strTags + ")";
+		ResultSet rs = execSql(query);
 
-			return true;
+		List<Item> mylist = new ArrayList<Item>();
+		if (rs.next()) {
+			mylist.add(getItemObj(rs));
 		}
 
-		public List<Item> getItems() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public List<Item> getItems(List<Tag> tags) throws SQLException {
-			String strTags = "";
-			int listSize = tags.size();
-			
-			if (listSize > 0) strTags = "'" + tags.get(0).toString() + "'";
-			
-			for (int i=1; i<listSize; i++) {
-				strTags += ", '" + tags.get(0).toString() + "'";
-			}
-
-			String query =
-					"SELECT * FROM items i, tags t, item_tags r " +
-					"WHERE i.itemid = r.itemid AND t.tagid = item_tags.tagid " +
-							"AND t.tag_name IN (" + strTags + ")";
-			ResultSet rs = execSql(query);
-			
-			List<Item> mylist = new ArrayList<Item>();
-			if (rs.next()) {
-				mylist.add(getItemObj(rs));
-			}
-			
-			return mylist;
-		}
+		return mylist;
+	}
 
 }
--- MarketPlace/src/mysqldao/TagMySQLDAO.java
+++ MarketPlace/src/mysqldao/TagMySQLDAO.java
@@ -2,6 +2,7 @@ package mysqldao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Tag;
@@ -9,43 +10,42 @@ import model.Tag;
 import dao.AbstractDAO;
 import dao.TagDAO;
 
-public class TagMySQLDAO extends AbstractDAO implements TagDAO{
-		
-		public TagMySQLDAO(){
-			mConnection = createConnection();
-		}
-		
-		private Tag createTagObj(ResultSet rs) throws SQLException {
-			Tag tag = null;
-			if (rs.next()) {
-				tag = new Tag(rs.getLong("tagid"), rs.getString("tag_name"));
-			}
-			return tag;
-		}
+public class TagMySQLDAO extends AbstractDAO implements TagDAO {
 
-		@Override
-		public String getTag(String tagName) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	public TagMySQLDAO() {
+		mConnection = createConnection();
+	}
 
-		@Override
-		public boolean deleteTag(String tagName) {
-			// TODO Auto-generated method stub
-			return false;
-		}
+	private Tag createTagObj(ResultSet rs) throws SQLException {
+		Tag tag = new Tag(rs.getLong("tagid"), rs.getString("tag_name"));
+		return tag;
+	}
 
-		@Override
-		public boolean createTag(String tagName) {
-			// TODO Auto-generated method stub
-			return false;
-		}
+	public boolean tagExists(String tagName) throws SQLException {
+		ResultSet rs = execSql("SELECT * FROM tags WHERE tag_name='" + tagName + "'");
+		if (rs.next()) return true;
+		else return false;
+	}
 
-		@Override
-		public List<Tag> getTags() {
-			
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
+	@Override
+	public boolean deleteTag(String tagName) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean createTag(String tagName) throws SQLException {
+		if (!tagExists(tagName))
+			execSql("INSERT INTO tags(tag_name) VALUES ('" + tagName + "')");
+		return true;
+	}
+
+	public List<Tag> getTags() throws SQLException {
+		ResultSet rs = execSql("SELECT * FROM Tags");
+		List<Tag> ret = new ArrayList<Tag>();
+		if (rs.next())
+			ret.add(createTagObj(rs));
+		return ret;
+	}
+
 }
