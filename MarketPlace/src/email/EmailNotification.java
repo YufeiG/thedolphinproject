fc83271523b18c0d6d7f8c8f494a52f9dc51b4a4
tinaliang22 <tinaliang22@hotmail.com>
2012-07-17 18:14:04 -0400

    implemented email notification

--- MarketPlace/src/email/EmailNotification.java
+++ MarketPlace/src/email/EmailNotification.java
@@ -1,14 +1,48 @@
 package email;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import model.Item;
 import model.User;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 public class EmailNotification {
 
 	public void sendMessageToUser(HashMap<User, List<Item>> list){
+		Iterator<User> it = list.keySet().iterator();
+		
+		while(it.hasNext()){
+			User currentUser = it.next();
+			List<Item> items = list.get(currentUser);
+			
+			StringBuffer sb = new StringBuffer();
+			
+			Iterator<Item> itemIt = items.iterator();
+			while(itemIt.hasNext()){
+				Item curItem = itemIt.next();
+				sb.append(curItem.getTitle() + " : " + curItem.getDescription());
+			}
+			
+			try {
+				SimpleEmail email = new SimpleEmail();
+				email.addTo(currentUser.getEmail());
+				email.setMsg(sb.toString());
+				email.setSubject("Wish List Match");
+				email.setFrom("uwaterloo.marketplace@gmail.com", "UW Marketplace");
+				//email.setHostName("smtp.googlemail.com");
+				//email.setSmtpPort(465);
+				//email.setAuthenticator(new DefaultAuthenticator("uwaterloo.marketplace@gmail.com", "cs446avenger"));
+				email.send();
+			
+			} catch (EmailException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 }
